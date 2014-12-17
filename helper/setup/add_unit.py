67d1d2a560d1602f801854302f018b7eb0f645d2
Liam Young <liam.young@canonical.com>
2014-12-17 10:05:25 +0000

    Fix up add_unit.py

--- helper/setup/add_unit.py
+++ helper/setup/add_unit.py
@@ -8,11 +8,12 @@ import argparse
 
 def main(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument("units", default=default_machines, nargs="*")
+    parser.add_argument("units", nargs="*")
+    options = parser.parse_args()
     unit_args = mojo_utils.parse_mojo_arg(options, 'units', multiargs=True)
     for unitreq in unit_args:
         service, count = unitreq.split(":")
-        add_unit(service, unit_num=count) 
+        mojo_utils.add_unit(service, unit_num=count) 
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
