8fc59838e1c006323af454a977233709cf3e28f0
Liam Young <liam.young@canonical.com>
2015-01-08 20:55:00 +0000

    Fix service referenced berfore assignment bug

--- helper/setup/delete_unit.py
+++ helper/setup/delete_unit.py
@@ -11,10 +11,10 @@ def main(argv):
     parser.add_argument("units", nargs="*")
     options = parser.parse_args()
     unit_args = mojo_utils.parse_mojo_arg(options, 'units', multiargs=True)
-    units = mojo_utils.get_juju_units(service=service)
     for unitreq in unit_args:
         if ':' in unitreq:
             service, unitcount = unitreq.split(':')
+            units = mojo_utils.get_juju_units(service=service)
             i = 0
             for unit in units:
                 if i >= int(unitcount):
@@ -22,6 +22,7 @@ def main(argv):
                 mojo_utils.delete_unit(unit)
                 i = i + 1
         elif '/' in unitreq:
+            units = mojo_utils.get_juju_units(service=unitreq.split('/')[0])
             if unitreq in units:
                 mojo_utils.delete_unit(unitreq)
 
