f730b54ce7cbddde8457aa17f075d65eb0a120ca
Liam Young <liam.young@canonical.com>
2015-01-09 09:20:00 +0000

    Remove hardcoded config file

--- helper/setup/keystone_setup.py
+++ helper/setup/keystone_setup.py
@@ -7,8 +7,7 @@ import utils.mojo_os_utils as mojo_os_utils
 def main(argv):
     overcloud_novarc = mojo_utils.get_overcloud_auth()
     keystone_client = mojo_os_utils.get_keystone_client(overcloud_novarc)
-    #user_config = mojo_utils.get_mojo_config('keystone_users.yaml')
-    user_config = '/home/ubuntu/mojo/mojo-openstack-specs/helper/setup/keystone_users.yaml'
+    user_config = mojo_utils.get_mojo_config('keystone_users.yaml')
     tenants = [user['tenant'] for user in user_config]
     mojo_os_utils.tenant_create(keystone_client, tenants)
     mojo_os_utils.user_create(keystone_client, user_config)
