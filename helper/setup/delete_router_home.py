1e613a208e82a62de2b93d4e4b16d4abd9abd178
Liam Young <liam.young@canonical.com>
2015-01-20 14:19:10 +0000

    Add symlincks for router checks

---
+++ helper/setup/delete_router_home.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import sys
+import utils.mojo_utils as mojo_utils
+import utils.mojo_os_utils as mojo_os_utils
+import logging
+import argparse
+
+
+def main(argv):
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("router", nargs="?")
+    options = parser.parse_args()
+    router_name = mojo_utils.parse_mojo_arg(options, 'router')
+    overcloud_novarc = mojo_utils.get_overcloud_auth() 
+    neutron_client = mojo_os_utils.get_neutron_client(overcloud_novarc)
+    router = neutron_client.list_routers(name=router_name)['routers'][0]
+    hosting_machine = neutron_client.list_l3_agent_hosting_routers(router=router['id'])['agents'][0]['host']
+    mojo_utils.delete_machine(hosting_machine)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
---
+++ helper/tests/check_guest_connectivity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import sys
+import utils.mojo_utils as mojo_utils
+import utils.mojo_os_utils as mojo_os_utils
+import logging
+import argparse
+
+
+def main(argv):
+    logging.basicConfig(level=logging.INFO)
+    overcloud_novarc = mojo_utils.get_overcloud_auth()
+    novac = mojo_os_utils.get_nova_client(overcloud_novarc)
+    mojo_os_utils.check_guest_connectivity(novac)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
