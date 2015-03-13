ef35a2aff6823423996dbb37717b5918f3023d8b
Liam Young <liam.young@canonical.com>
2015-03-13 10:47:19 +0000

    Add rabbit support

--- specs/dev/leadership/icehouse/leader_chaos.py
+++ specs/dev/leadership/icehouse/leader_chaos.py
@@ -9,13 +9,24 @@ import xml.dom.minidom
 import re
 import ast
 
-def rabbit_status(unit)
-    output = mojo_utils.remote_run(unit, remote_cmd='rabbitmqctl -q cluster_status')
+def rabbit_unit_status(unit):
+    output = mojo_utils.remote_run(unit, remote_cmd='rabbitmqctl -q cluster_status')[0]
     output = output.replace('\n', '')
     matchObj = re.search( r'running_nodes,(.*)}, {partitions', output)
-    node_list = ast.literal_eval(matchObj.group(1))
-    return [unit.replace('rabbit@' for unit in node_list ]
+    machine_numbers = []
+    for machine in ast.literal_eval(matchObj.group(1)):
+        machine_numbers.append(int(machine.split('-')[-1]))
+    return machine_numbers
 
+def rabbit_status():
+    juju_units = mojo_utils.get_juju_units(service='rabbitmq-server')
+    machine_numbers = get_machine_numbers('rabbitmq-server')
+    for unit in juju_units:
+        units = rabbit_unit_status(unit)
+        units.sort()
+        if machine_numbers != units:
+            print "%s != %s" % (str(machine_numbers), str(units))
+            raise Exception('Mismatch on rabbit status for on unit %s' % (unit))
 
 def unit_crm_online(unit):
     output = mojo_utils.remote_run(unit, remote_cmd='crm_mon -X')
@@ -29,6 +40,14 @@ def unit_crm_online(unit):
     online_units.sort()
     return online_units
 
+def get_machine_numbers(service):
+    juju_units = mojo_utils.get_juju_units(service=service)
+    machine_numbers = []
+    for unit in juju_units:
+        machine_numbers.append(mojo_utils.convert_unit_to_machinename(unit))
+    machine_numbers.sort()
+    return machine_numbers
+
 def check_crm_status(service):
     machine_numbers = []
     juju_units = mojo_utils.get_juju_units(service=service)
@@ -38,11 +57,14 @@ def check_crm_status(service):
     if output[0].rstrip() == "Not Found":
         return
     for unit in juju_units:
-        machine_numbers.append(mojo_utils.convert_unit_to_machinename(unit))
-    machine_numbers.sort()
-    for unit in juju_units:
-        if machine_numbers != unit_crm_online(unit):
+        if get_machine_numbers(service) != unit_crm_online(unit):
             raise Exception('Mismatch on crm status for service %s on unit %s' % (service, unit))
+
+def check_cluster_status(service):
+    if service == 'rabbitmq-service':
+        rabbit_status()
+    else:
+        check_crm_status(service)
 
 def main(argv):
     logging.basicConfig(level=logging.INFO)
@@ -58,7 +80,7 @@ def main(argv):
         mojo_os_utils.delete_juju_leader(doomed_service)
         mojo_utils.juju_check_hooks_complete()
         mojo_utils.juju_status_check_and_wait()
-        check_crm_status(doomed_service)
+        check_cluster_status(doomed_service)
         mojo_utils.add_unit(doomed_service, unit_num=1)
         mojo_utils.juju_status_check_and_wait()
         check_crm_status(doomed_service)
