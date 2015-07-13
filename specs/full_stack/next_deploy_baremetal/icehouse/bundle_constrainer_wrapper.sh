fa7e283d99670b2c9d173ddacaf2b7b87b59fb2a
Ryan Beisner <ryan.beisner@canonical.com>
2015-07-13 21:01:14 +0000

    update

--- specs/full_stack/next_deploy_baremetal/icehouse/bundle_constrainer_wrapper.sh
+++ specs/full_stack/next_deploy_baremetal/icehouse/bundle_constrainer_wrapper.sh
@@ -3,6 +3,11 @@
 #  Optionally consume juju tags passed by environment variable, such
 #  as architecture or maas tags.
 
+env | egrep 'osci|OSCI|juju|JUJU|mojo|MOJO' | sort
+
 if [[ -n "$UOSCI_JUJU_TAGS" ]]; then
+  echo "Bundle constrainer not triggered on: ${UOSCI_JUJU_TAGS}"
   scripts/bundle_constrainer.py -yd -i baremetal7.yaml -o baremetal7.yaml --constraints "$UOSCI_JUJU_TAGS" -e "ceilometer-agent,cinder-ceph,neutron-openvswitch"
+else
+  echo "Bundle constrainer not triggered."
 fi
