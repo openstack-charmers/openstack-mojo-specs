c60af90e6b0fba5feaaf3770efbf044fdbde7807
David Ames <david.ames@canonical.com>
2016-02-27 00:29:46 +0000

    Fix lint

--- helper/setup/bug1364215.py
+++ helper/setup/bug1364215.py
@@ -5,7 +5,7 @@ import utils.mojo_utils as mojo_utils
 
 def main(argv):
     for unit in mojo_utils.get_juju_units(service='neutron-gateway'):
-        cmd='sudo service neutron-plugin-openvswitch-agent restart'
+        cmd = 'sudo service neutron-plugin-openvswitch-agent restart'
         mojo_utils.remote_run(unit, remote_cmd=cmd)
 
 if __name__ == "__main__":
