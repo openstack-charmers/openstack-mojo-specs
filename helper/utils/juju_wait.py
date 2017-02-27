d2284f5bf8c25ea1a7163d8f136af5f0b43085e2
David Ames <david.ames@canonical.com>
2017-02-27 15:14:57 -0800

    Use upstream juju_wait

--- helper/utils/juju_wait.py
+++ helper/utils/juju_wait.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
-# NOTE(beisner):  this is a fork of juju-wait which enables workload
-# status blocking.  ie. If a unit is in a blocked state, keep waiting.
-#   From:  https://code.launchpad.net/~1chb1n/juju-wait/workload-status
-#   Re:    https://bugs.launchpad.net/juju-wait/+bug/1553312
-
 # This file is part of juju-wait, a juju plugin to wait for environment
 # steady state.
 #
@@ -32,8 +27,10 @@ import subprocess
 import sys
 from textwrap import dedent
 import time
+import yaml
 
-import kiki
+
+__version__ = '2.4.3'
 
 
 class DescriptionAction(argparse.Action):
@@ -44,6 +41,8 @@ class DescriptionAction(argparse.Action):
 class EnvironmentAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         os.environ['JUJU_ENV'] = values[0]
+        # also set JUJU_MODEL environment variable for juju 2.x
+        os.environ['JUJU_MODEL'] = values[0]
 
 
 def parse_ts(ts):
@@ -77,11 +76,42 @@ def run_or_die(cmd, env=None):
     return out
 
 
+def juju_exe():
+    # Allow override using environment variable(s)
+    if os.environ.get('JUJU_BINARY'):
+        # JUJU_BINARY can be a full path: /path/to/juju
+        juju = os.environ.get('JUJU_BINARY')
+    elif os.environ.get('JUJU_VERSION'):
+        # JUJU_VERSION can specify just the version
+        # and select from juju-$VER in PATH
+        ver = (".".join(os.environ.get('JUJU_VERSION')
+               .split('-')[0].split('.')[:2]))
+        juju = 'juju-{}'.format(ver)
+    else:
+        # Default to juju in PATH
+        juju = 'juju'
+    return juju
+
+
 def juju_run(unit, cmd, timeout=None):
     if timeout is None:
         timeout = 6 * 60 * 60
-    return run_or_die([kiki.cmd(), 'run', '--timeout={}s'.format(timeout),
+    return run_or_die([juju_exe(), 'run', '--timeout={}s'.format(timeout),
                        '--unit', unit, cmd])
+
+
+def juju_run_many(units, cmd, timeout=None):
+    units = list(units)
+    if not units:
+        return {}
+    args = [juju_exe(), 'run', '--format=yaml', '--unit', ','.join(units)]
+    if timeout is not None:
+        args.append('--timeout={}s'.format(timeout))
+    args.extend(['--', cmd])
+    out = yaml.safe_load(run_or_die(args))
+    # ReturnCode is omitted from the YAML when 0, so assume absence is
+    # success.
+    return {d['UnitId']: (d.get('ReturnCode', 0), d['Stdout']) for d in out}
 
 
 def get_status():
@@ -89,7 +119,7 @@ def get_status():
     # using the environment variable.
     env = os.environ.copy()
     env['TZ'] = 'UTC'
-    json_status = run_or_die([kiki.cmd(), 'status', '--format=json'], env=env)
+    json_status = run_or_die([juju_exe(), 'status', '--format=json'], env=env)
     if json_status is None:
         return None
     return json.loads(json_status)
@@ -101,9 +131,25 @@ def get_log_tail(unit, timeout=None):
     return juju_run(unit, cmd, timeout=timeout)
 
 
-def get_is_leader(unit, timeout=None):
-    raw = juju_run(unit, 'is-leader --format=json', timeout=timeout)
-    return json.loads(raw)
+def leadership_poll(units):
+    is_leader_results = juju_run_many(units, 'is-leader --format=json')
+    unit_map = {}
+    failed = False
+    for unit, (return_code, stdout) in is_leader_results.items():
+        if return_code != 0:
+            logging.error("{} has failed. Unable to run leadership check."
+                          "".format(unit))
+            failed = True
+            continue
+        try:
+            unit_map[unit] = json.loads(stdout)
+        except ValueError:
+            logging.error("{} has failed. Insane output from commands."
+                          "".format(unit))
+            failed = True
+    if failed:
+        raise JujuWaitException(44)
+    return unit_map
 
 
 # Juju 1.24+ provides us with the timestamp the status last changed.
@@ -121,6 +167,10 @@ IDLE_CONFIRMATION = timedelta(seconds=15)
 # values indicate not-ready workload states.
 WORKLOAD_OK_STATES = ['active', 'unknown']
 
+# If any unit ever reaches one of the following workload status
+# values, consider that fatal and raise.
+WORKLOAD_ERROR_STATES = ['error']
+
 
 def wait_cmd(args=sys.argv[1:]):
     description = dedent("""\
@@ -129,24 +179,30 @@ def wait_cmd(args=sys.argv[1:]):
         The environment is considered in a steady state once all hooks
         have completed running and there are no hooks queued to run,
         on all units.
-
-        If you need a timeout, use the timeout(1) tool.
         """)
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('-e', '--environment', metavar="ENV", type=str,
+    parser.add_argument('-e', '-m', '--environment', '--model',
+                        metavar="MODEL", type=str,
                         action=EnvironmentAction, nargs=1)
     parser.add_argument('--description', action=DescriptionAction, nargs=0)
     parser.add_argument('-q', '--quiet', dest='quiet',
                         action='store_true', default=False)
+    # XXX Delta from upstream: default = True
     parser.add_argument('-v', '--verbose', dest='verbose',
-                        action='store_true', default=False)
+                        action='store_true', default=True)
+    # XXX Delta from upstream: default = True
     parser.add_argument('-w', '--workload', dest='wait_for_workload',
                         help='Wait for unit workload status active state',
-                        action='store_true', default=False)
+                        action='store_true', default=True)
+    # XXX Delta from upstream: default = 2700
     parser.add_argument('-t', '--max_wait', dest='max_wait',
                         help='Maximum time to wait for readiness (seconds)',
-                        action='store', default=None)
+                        action='store', default=2700)
+    parser.add_argument('--version', default=False, action='store_true')
     args = parser.parse_args(args)
+
+    if args.version:
+        parser.exit(0, __version__ + '\n')
 
     # Parser did not exit, so continue.
     logging.basicConfig()
@@ -179,11 +235,19 @@ def reset_logging():
 
     Reset the environment log settings to match default juju stable.
     """
-    run_or_die([kiki.cmd(), kiki.model_config(),
+    run_or_die([juju_exe(), 'set-environment',
                 'logging-config=juju=WARNING;unit=INFO'])
 
 
 def wait(log=None, wait_for_workload=False, max_wait=None):
+    # Note that wait_for_workload is only useful as a workaround for
+    # broken setups. It is impossible for a charm to report that it has
+    # completed setup, because a charm has no information about units
+    # still being provisioned or relations that have not yet been joined.
+    # A charm might report an active state, but there may still be hours
+    # worth of setup hooks to run before it is complete. wait_for_workload
+    # does allow you to ignore infinite hook loops and other similar bugs
+    # in your charms.
     if log is None:
         log = logging.getLogger()
 
@@ -194,13 +258,10 @@ def wait(log=None, wait_for_workload=False, max_wait=None):
     epoch_started = time.time()
     ready_since = None
     logging_reset = False
-    model_version = ''
 
     while True:
         status = get_status()
         ready = True
-        if 'model' in status:
-            model_version = status['model']['version']
 
         # If defined, fail if max_wait is exceeded
         epoch_elapsed = time.time() - epoch_started
@@ -210,8 +271,8 @@ def wait(log=None, wait_for_workload=False, max_wait=None):
             raise JujuWaitException(44)
 
         # If there is a dying service, environment is not quiescent.
-        for sname, service in sorted(status.get(kiki.applications(),
-                                                {}).items()):
+        services = status.get('services') or status.get('applications', {})
+        for sname, service in sorted(services.items()):
             if service.get('life') in ('dying', 'dead'):
                 logging.debug('{} is dying'.format(sname))
                 ready = False
@@ -222,53 +283,96 @@ def wait(log=None, wait_for_workload=False, max_wait=None):
         # logs sniffed because they are running Juju 1.23 or earlier.
         ready_units = {}
 
-        # Flattened agent and workload status for all units and subordinates
-        # that provide it. Note that 'agent status' is only available in
-        # Juju 1.24 and later. This is easily confused with 'agent state'
-        # which is available in earlier versions of Juju.
+        # Flattened agent and workload status and leadership for all units
+        # and subordinates that provide it. Note that 'agent status' is only
+        # available in Juju 1.24 and later. This is easily confused with
+        # 'agent state' which is available in earlier versions of Juju.
         workload_status = {}
         agent_status = {}
         agent_version = {}
-        for sname, service in status.get(kiki.applications(), {}).items():
+        unit_leadership = {}
+        for sname, service in services.items():
             for uname, unit in service.get('units', {}).items():
                 all_units.add(uname)
-                agent_version[uname] = unit.get('agent-version') or model_version
+                unit_leadership[uname] = unit.get('leader', None)
+                if 'agent-version' in unit:
+                    agent_version[uname] = unit.get('agent-version')
+                elif 'juju-status' in unit and ('version'
+                                                in unit['juju-status']):
+                    # agent-version disappeared and was replaced by
+                    # a subkey of juju-status some time during the Juju
+                    # 2.0 beta cycle.
+                    agent_version[uname] = unit['juju-status']['version']
                 if ('workload-status' in unit and
                         'current' in unit['workload-status']):
                     workload_status[uname] = unit['workload-status']
 
                 if 'agent-status' in unit and unit['agent-status'] != {}:
                     agent_status[uname] = unit['agent-status']
-                elif 'juju-status' in unit and 'current' in unit['juju-status']:
+                elif 'juju-status' in unit and unit['juju-status'] != {}:
+                    # agent-status was renamed to juju-status some time
+                    # during the Juju 2.0 beta cycle.
                     agent_status[uname] = unit['juju-status']
                 else:
                     ready_units[uname] = unit  # Schedule for sniffing.
                 for subname, sub in unit.get('subordinates', {}).items():
+                    unit_leadership[subname] = sub.get('leader', None)
                     if ('workload-status' in sub and
                             'current' in sub['workload-status']):
                         workload_status[subname] = sub['workload-status']
 
-                    agent_version[subname] = sub.get('agent-version') or model_version
+                    if 'agent-version' in sub:
+                        agent_version[subname] = sub['agent-version']
+                    elif 'juju-status' in sub and ('version' in
+                                                   sub['juju-status']):
+                        agent_version[subname] = sub['juju-status']['version']
                     if 'agent-status' in sub and unit['agent-status'] != {}:
                         agent_status[subname] = sub['agent-status']
-                    elif 'juju-status' in unit and 'current' in unit['juju-status']:
-                        agent_status[uname] = unit['juju-status']
+                    elif 'juju-status' in sub and unit['juju-status'] != {}:
+                        # agent-status was renamed to juju-status some time
+                        # during the Juju 2.0 beta cycle.
+                        agent_status[subname] = sub['juju-status']
                     else:
                         ready_units[subname] = sub  # Schedule for sniffing.
 
         for uname, wstatus in sorted(workload_status.items()):
+            if not ('current' in wstatus and 'since' in wstatus):
+                ready = False
+                continue
             current = wstatus['current']
             since = parse_ts(wstatus['since'])
+
+            # Check workload status
             if current not in WORKLOAD_OK_STATES and wait_for_workload:
                 logging.debug('{} workload status is {} since '
                               '{}Z'.format(uname, current, since))
                 ready = False
 
+            # Fail and raise if workload state is ever in error
+            if current in WORKLOAD_ERROR_STATES and wait_for_workload:
+                logging.error('{} failed: workload status is '
+                              '{}'.format(uname, current))
+                ready = False
+                raise JujuWaitException(1)
+
         for uname, astatus in sorted(agent_status.items()):
+            if not ('current' in astatus and 'since' in astatus):
+                ready = False
+                continue
             current = astatus['current']
             since = parse_ts(astatus['since'])
-            if current != 'idle':
-                logging.debug('{} agent status is {} since '
+            if 'message' in astatus:
+                message = astatus['message']
+            else:
+                message = ''
+
+            # Check agent status
+            if (current == 'executing' and
+                    message == 'running update-status hook'):
+                # update-status is an idle hook event
+                pass
+            elif current != 'idle':
+                logging.debug('{} juju agent status is {} since '
                               '{}Z'.format(uname, current, since))
                 ready = False
 
@@ -276,14 +380,11 @@ def wait(log=None, wait_for_workload=False, max_wait=None):
         logs = {}
 
         # Sniff logs of units that don't provide agent-status, if necessary.
+        # This section can go when we drop support for Juju < 1.24.
         for uname, unit in sorted(ready_units.items()):
             dying = unit.get('life') in ('dying', 'dead')
-            if kiki.version() < 2:
-                agent_state = unit.get('agent-state')
-                agent_state_info = unit.get('agent-state-info')
-            else:
-                agent_state = unit.get('workload-status').get('current')
-                agent_state_info = unit.get('workload-status').get('message')
+            agent_state = unit.get('agent-state')
+            agent_state_info = unit.get('agent-state-info')
             if dying:
                 logging.debug('{} is dying'.format(uname))
                 ready = False
@@ -291,8 +392,8 @@ def wait(log=None, wait_for_workload=False, max_wait=None):
                 logging.error('{} failed: {}'.format(uname, agent_state_info))
                 ready = False
                 raise JujuWaitException(1)
-            elif agent_state not in ['started', 'unknown', 'active']:
-                logging.debug('{} is not ready: {}'.format(uname, agent_state))
+            elif agent_state != 'started':
+                logging.debug('{} is {}'.format(uname, agent_state))
                 ready = False
             elif ready:
                 # We only start grabbing the logs once all the units
@@ -307,12 +408,10 @@ def wait(log=None, wait_for_workload=False, max_wait=None):
                 if logs[uname] == prev_logs.get(uname):
                     logging.debug('{} is idle - no hook activity'
                                   ''.format(uname))
-                    ready = True
                 else:
-                    logging.debug('{} is still executing: {}'
+                    logging.debug('{} is active: {}'
                                   ''.format(uname, logs[uname].strip()))
                     ready = False
-
 
         if ready:
             # We are never ready until this check has been running until
@@ -322,37 +421,61 @@ def wait(log=None, wait_for_workload=False, max_wait=None):
             # a chance to fire any hooks it is going to.
             if ready_since is None:
                 ready_since = datetime.utcnow()
+                ready = False
             elif ready_since + IDLE_CONFIRMATION < datetime.utcnow():
                 logging.info('All units idle since {}Z ({})'
                              ''.format(ready_since,
                                        ', '.join(sorted(all_units))))
-                ready = True
+            else:
+                ready = False
         else:
             ready_since = None
             ready = False
 
         # Ensure every service has a leader. If there is no leader, then
         # one will be appointed soon and hooks should kick off.
+        # Run last as it can take quite awhile on environments with a
+        # large number of services.
         if ready:
+            for uname, ver in agent_version.items():
+                if ver and LooseVersion(ver) < LooseVersion('1.24'):
+                    # Leadership was added in 1.24, so short-circuit to true
+                    # anything older.
+                    unit_leadership[uname] = True
+                elif (ver and LooseVersion(ver) >= LooseVersion('2.1') and
+                      unit_leadership[uname] is None):
+                    # Leadership is set in the status output
+                    # only when leader = True
+                    # Avoid the costly juju run when unnecessary
+                    unit_leadership[uname] = False
+
+            # Ask all the other units in parallel whether they're the
+            # leader if leader is not set.
+            unit_leadership.update(leadership_poll(
+                uname for uname, leader in unit_leadership.items()
+                if leader is None))
+
+            # Aggregate and log the leader status by service.
             services = set()
             services_with_leader = set()
-            for uname, version in agent_version.items():
+            for uname, is_leader in unit_leadership.items():
                 sname = uname.split('/', 1)[0]
                 services.add(sname)
-                if 'leader' in unit and unit['leader']:
-                    services_with_leader.add(sname)
-                    logging.debug('{} is lead by {}'.format(sname, uname))
-                elif (sname not in services_with_leader and version and
-                    (LooseVersion(version) <= LooseVersion('1.23') or
-                        get_is_leader(uname) is True)):
+                if sname not in services_with_leader and is_leader:
                     services_with_leader.add(sname)
                     logging.debug('{} is lead by {}'.format(sname, uname))
             for sname in services:
+                # Note that we only collected services that have 1 or more
+                # units. A service with no units will not have a leader
+                # and this is fine and juju-wait should ignore it.
                 if sname not in services_with_leader:
                     logging.info('{} does not have a leader'.format(sname))
+                    ready_since = None
                     ready = False
-                 else:
-                     return
+
+        # If everything looks good, return.
+        if ready:
+            return
 
         prev_logs = logs
         time.sleep(4)
@@ -363,7 +486,8 @@ if __name__ == '__main__':
     # Most installations will be using the setuptools generated
     # launchers.
     script = os.path.basename(sys.argv[0])
-    if script == 'juju-wait':
+    # XXX Delta from upstream
+    if script == 'juju-wait' or 'juju_wait':
         sys.exit(wait_cmd())
     else:
         raise RuntimeError('Unknown script {}'.format(script))
