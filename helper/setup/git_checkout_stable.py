b802706c057e52d3af34eb282d993814ecf1229d
Ryan Beisner <ryan.beisner@canonical.com>
2017-09-06 09:48:42 -0500

    Update stable branch to 17.02

--- helper/setup/git_checkout_stable.py
+++ helper/setup/git_checkout_stable.py
@@ -7,7 +7,7 @@ import utils.mojo_utils as mojo_utils
 def main(argv):
     log = logging.getLogger()
     log.setLevel(logging.DEBUG)
-    mojo_utils.git_checkout_all('stable/16.10')
+    mojo_utils.git_checkout_all('stable/17.02')
 
 
 if __name__ == "__main__":
