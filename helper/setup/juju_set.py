a3d8fdd4467c33d7d0d7fa93c867304bfe46caa2
Liam Young liam.young@canonical.com <>
2017-11-23 11:37:21 +0000

    Fix cluster check increase
    When the cluster count in increased the charms workload status reports
    that there are insufficent units which blocks the juju_set function.
    
    This change adds the WAIT option to juju_set.py so the manifest can
    stop the wait on workload status. The manifests have been updated to
    set the WAIT option

--- helper/setup/juju_set.py
+++ helper/setup/juju_set.py
@@ -8,12 +8,18 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("--service")
     parser.add_argument("--kv")
+    parser.add_argument("--wait")
     options = parser.parse_args()
     service = mojo_utils.parse_mojo_arg(options, 'service')
     kv = mojo_utils.parse_mojo_arg(options, 'kv')
+    wait = mojo_utils.parse_mojo_arg(options, 'wait')
+    print("Wait: {}".format(wait))
+    if wait is not None:
+        wait = wait == 'True'
     print("Service: {}".format(service))
     print("Option: {}".format(kv))
-    mojo_utils.juju_set(service, kv)
+    print("Wait: {}".format(wait))
+    mojo_utils.juju_set(service, kv, wait=wait)
 
 
 if __name__ == "__main__":
