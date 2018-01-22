7b337d38cfd2d287c397dbfcbe544783995b7e50
Liam Young liam.young@canonical.com <>
2018-01-22 09:41:18 +0000

    Explicitly check record name

--- helper/tests/validate_designate.py
+++ helper/tests/validate_designate.py
@@ -17,11 +17,11 @@ def main(argv):
     overcloud_novarc = mojo_utils.get_overcloud_auth()
     keystone_session = mojo_os_utils.get_keystone_session(overcloud_novarc,
                                                           scope='PROJECT')
-    novac = mojo_os_utils.get_nova_session_client(keystone_session)
+    nova_client = mojo_os_utils.get_nova_session_client(keystone_session)
     des_client = mojo_os_utils.get_designate_session_client(keystone_session,
                                                             all_tenants=True)
     nova_domain = mojo_utils.juju_get('designate', 'nova-domain')
-    for server in novac.servers.list():
+    for server in nova_client.servers.list():
         for addr_info in server.addresses['private']:
             if addr_info['OS-EXT-IPS:type'] == 'floating':
                 mojo_os_utils.check_dns_entry_in_bind(
@@ -29,10 +29,27 @@ def main(argv):
                     '{}.{}'.format(server.name, domain_name))
             # Test legacy notifications based records
             if addr_info['OS-EXT-IPS:type'] == 'fixed' and nova_domain:
+                keystone_client = mojo_os_utils.get_keystone_session_client(
+                    keystone_session)
+
+                record_ctxt = {
+                    'hostname': server.name,
+                    'zone': nova_domain}
+
+                for project in keystone_client.projects.list():
+                    if project.name == overcloud_novarc['OS_TENANT_NAME']:
+                        record_ctxt['project_id'] = project.id
+                        record_ctxt['tenant_id'] = project.id
+
+                record_format = mojo_utils.juju_get(
+                    'designate',
+                    'nova-record-format')
+                record_name = record_format % record_ctxt
                 mojo_os_utils.check_dns_entry_in_designate(
                     des_client,
                     addr_info['addr'],
-                    nova_domain)
+                    nova_domain,
+                    record_name=record_name)
 
 
 if __name__ == "__main__":
