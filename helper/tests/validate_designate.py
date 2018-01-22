982e4f9f8c25aad964a461eb2fdb6a7c9a89dcf7
Liam Young liam.young@canonical.com <>
2018-01-22 08:05:07 +0000

    Add checks for designate nova legacy records

--- helper/tests/validate_designate.py
+++ helper/tests/validate_designate.py
@@ -18,12 +18,22 @@ def main(argv):
     keystone_session = mojo_os_utils.get_keystone_session(overcloud_novarc,
                                                           scope='PROJECT')
     novac = mojo_os_utils.get_nova_session_client(keystone_session)
+    des_client = mojo_os_utils.get_designate_session_client(keystone_session,
+                                                            all_tenants=True)
+    nova_domain = mojo_utils.juju_get('designate', 'nova-domain')
     for server in novac.servers.list():
         for addr_info in server.addresses['private']:
             if addr_info['OS-EXT-IPS:type'] == 'floating':
                 mojo_os_utils.check_dns_entry_in_bind(
                     addr_info['addr'],
                     '{}.{}'.format(server.name, domain_name))
+            # Test legacy notifications based records
+            if addr_info['OS-EXT-IPS:type'] == 'fixed' and nova_domain:
+                mojo_os_utils.check_dns_entry_in_designate(
+                    des_client,
+                    addr_info['addr'],
+                    nova_domain)
+
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
