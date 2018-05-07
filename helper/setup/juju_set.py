febea0349878fa4d9bf924d29cf7f3e7c1795ede
David Ames <david.ames@canonical.com>
2018-05-07 15:18:53 -0700

    Standardize env python3

--- helper/setup/juju_set.py
+++ helper/setup/juju_set.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import utils.mojo_utils as mojo_utils
 import argparse
--- helper/setup/tempest_test.py
+++ helper/setup/tempest_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import utils.mojo_utils as mojo_utils
 import utils.mojo_os_utils as mojo_os_utils
